06d161c797ea28aab121418130c818fd6142c442
Luca Nora <luca.nora@gmail.com>
2022-08-02 15:54:04 +0000

    Misc fixes

--- backend/reddit/api/types.go
+++ backend/reddit/api/types.go
@@ -1,20 +1,18 @@
 package api
 
-import (
-)
-
 type ListItems struct {
 	Data []Item `json:"data"`
 }
 
 type Item struct {
 	Author     string `json:"author"`
+	Subreddit  string `json:"subreddit"`
 	CreatedUtc int64  `json:"created_utc"`
 	Id         string `json:"id"`
 	Domain     string `json:"domain"`
 	PostHint   string `json:"post_hint"`
 	Url        string `json:"url"`
-	Preview struct {
+	Preview    struct {
 		Images []struct {
 			ID string `json:"id"`
 		} `json:"images"`
--- backend/reddit/reddit.go
+++ backend/reddit/reddit.go
@@ -15,12 +15,14 @@ import (
 
 	"github.com/rclone/rclone/backend/reddit/api"
 	"github.com/rclone/rclone/fs"
+	"github.com/rclone/rclone/fs/config"
 	"github.com/rclone/rclone/fs/config/configmap"
 	"github.com/rclone/rclone/fs/config/configstruct"
 	"github.com/rclone/rclone/fs/fserrors"
 	"github.com/rclone/rclone/fs/fshttp"
 	"github.com/rclone/rclone/fs/hash"
 	"github.com/rclone/rclone/fs/log"
+	"github.com/rclone/rclone/lib/dircache"
 	"github.com/rclone/rclone/lib/pacer"
 	"github.com/rclone/rclone/lib/rest"
 )
@@ -41,6 +43,11 @@ func init() {
 	fsi := &fs.RegInfo{
 		Name:  "reddit",
 		NewFs: NewFs,
+		Options: []fs.Option{{
+			Name:     "no_head",
+			Default:  false,
+			Advanced: true,
+		}},
 
 		MetadataInfo: &fs.MetadataInfo{
 			System: map[string]fs.MetadataHelp{
@@ -56,9 +63,16 @@ func init() {
 
 // Options defines the configuration for this backend
 type Options struct {
-	Subreddit string `config:"subreddit"`
-	Author    string `config:"author"`
-	NoHead    bool   `config:"no_head"`
+	Subreddit  string `config:"subreddit"`
+	Author     string `config:"author"`
+	NoHead     bool   `config:"no_head"`
+	Checkpoint string `config:"checkpoint"`
+}
+
+type User struct {
+	before     int64
+	lastBefore int64
+	entries    fs.DirEntries
 }
 
 // Fs stores the interface to the remote HTTP files
@@ -72,8 +86,12 @@ type Fs struct {
 	//endpointURL string // endpoint as a string
 	srv        *rest.Client
 	pacer      *fs.Pacer
-	cache      map[string]string
+	cache      map[string]bool
 	httpClient *http.Client
+	dirCache   *dircache.DirCache // Map of directory path to directory id
+	users      map[string]User
+	//usersData  map[string]fs.DirEntries
+	subreddits map[string]string
 }
 
 // Object is a remote object that has been stat'd (so it exists, but is not necessarily open for reading)
@@ -100,82 +118,6 @@ type Object struct {
 	return nil
 } */
 
-// getFsEndpoint decides if url is to be considered a file or directory,
-// and returns a proper endpoint url to use for the fs.
-/* func getFsEndpoint(ctx context.Context, client *http.Client, url string, opt *Options) (string, bool) {
-	// If url ends with '/' it is already a proper url always assumed to be a directory.
-	if url[len(url)-1] == '/' {
-		return url, false
-	}
-
-	// If url does not end with '/' we send a HEAD request to decide
-	// if it is directory or file, and if directory appends the missing
-	// '/', or if file returns the directory url to parent instead.
-	createFileResult := func() (string, bool) {
-		fs.Debugf(nil, "If path is a directory you must add a trailing '/'")
-		parent, _ := path.Split(url)
-		return parent, true
-	}
-	createDirResult := func() (string, bool) {
-		fs.Debugf(nil, "To avoid the initial HEAD request add a trailing '/' to the path")
-		return url + "/", false
-	}
-
-	// If HEAD requests are not allowed we just have to assume it is a file.
-	if opt.NoHead {
-		fs.Debugf(nil, "Assuming path is a file as --http-no-head is set")
-		return createFileResult()
-	}
-
-	// Use a client which doesn't follow redirects so the server
-	// doesn't redirect http://host/dir to http://host/dir/
-	noRedir := *client
-	noRedir.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		return http.ErrUseLastResponse
-	}
-	req, err := http.NewRequestWithContext(ctx, "HEAD", url, nil)
-	if err != nil {
-		fs.Debugf(nil, "Assuming path is a file as HEAD request could not be created: %v", err)
-		return createFileResult()
-	}
-	addHeaders(req, opt)
-	res, err := noRedir.Do(req)
-
-	if err != nil {
-		fs.Debugf(nil, "Assuming path is a file as HEAD request could not be sent: %v", err)
-		return createFileResult()
-	}
-	if res.StatusCode == http.StatusNotFound {
-		fs.Debugf(nil, "Assuming path is a directory as HEAD response is it does not exist as a file (%s)", res.Status)
-		return createDirResult()
-	}
-	if res.StatusCode == http.StatusMovedPermanently ||
-		res.StatusCode == http.StatusFound ||
-		res.StatusCode == http.StatusSeeOther ||
-		res.StatusCode == http.StatusTemporaryRedirect ||
-		res.StatusCode == http.StatusPermanentRedirect {
-		redir := res.Header.Get("Location")
-		if redir != "" {
-			if redir[len(redir)-1] == '/' {
-				fs.Debugf(nil, "Assuming path is a directory as HEAD response is redirect (%s) to a path that ends with '/': %s", res.Status, redir)
-				return createDirResult()
-			}
-			fs.Debugf(nil, "Assuming path is a file as HEAD response is redirect (%s) to a path that does not end with '/': %s", res.Status, redir)
-			return createFileResult()
-		}
-		fs.Debugf(nil, "Assuming path is a file as HEAD response is redirect (%s) but no location header", res.Status)
-		return createFileResult()
-	}
-	if res.StatusCode < 200 || res.StatusCode > 299 {
-		// Example is 403 (http.StatusForbidden) for servers not allowing HEAD requests.
-		fs.Debugf(nil, "Assuming path is a file as HEAD response is an error (%s)", res.Status)
-		return createFileResult()
-	}
-
-	fs.Debugf(nil, "Assuming path is a file as HEAD response is success (%s)", res.Status)
-	return createFileResult()
-} */
-
 // NewFs creates a new Fs object from the name and root. It connects to
 // the host specified in the config file.
 func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, error) {
@@ -197,11 +139,16 @@ func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, e
 		httpClient: client,
 		srv:        rest.NewClient(client).SetRoot(rootURL),
 		pacer:      fs.NewPacer(ctx, pacer.NewDefault(pacer.MinSleep(minSleep), pacer.MaxSleep(maxSleep), pacer.DecayConstant(decayConstant))),
-		cache:      map[string]string{},
+		cache:      map[string]bool{},
+		users:      map[string]User{},
+		//usersData:  map[string]fs.DirEntries{},
+		subreddits: map[string]string{},
 	}
 	f.features = (&fs.Features{
 		CanHaveEmptyDirectories: true,
 	}).Fill(ctx, f)
+
+	f.dirCache = dircache.New(root, root, f)
 
 	return f, nil
 }
@@ -306,7 +253,7 @@ func getGfyCat(f *Fs, ctx context.Context, name string) (u string) {
 	return gfycat.GfyItem.Mp4URL
 }
 
-func getPushshift(f *Fs, ctx context.Context, subreddit, author string, before int64) (items []api.Item, err error) {
+func (f *Fs) getPushshift(ctx context.Context, subreddit, author string, before int64) (items []api.Item, err error) {
 	defer log.Trace(f, "subreddit=%v,author=%v,before=%v", subreddit, author, before)
 	var (
 		data *api.ListItems
@@ -320,7 +267,7 @@ func getPushshift(f *Fs, ctx context.Context, subreddit, author string, before i
 	if author != "" {
 		values.Set("author", author)
 	}
-	values.Set("size", "100")
+	values.Set("size", "25")
 	values.Set("sort", "desc")
 	values.Set("before", strconv.FormatInt(before, 10))
 
@@ -341,35 +288,10 @@ func getPushshift(f *Fs, ctx context.Context, subreddit, author string, before i
 	return data.Data, nil
 }
 
-// List the objects and directories in dir into entries.  The
-// entries can be returned in any order but should be for a
-// complete directory.
-//
-// dir should be "" to list the root, and should not have
-// trailing slashes.
-//
-// This should return ErrDirNotFound if the directory isn't
-// found.
-func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err error) {
-	var (
-		data      []api.Item
-		oldBefore int64
-	)
-
-	if err != nil {
-		return nil, err
-	}
-
-	
-	lastBefore := time.Now().AddDate(0, -1, 0).Unix()
-	for before := time.Now().Unix(); (before >= lastBefore) && (oldBefore != before); before = data[len(data)-1].CreatedUtc {
-		oldBefore = before
-		data2, _ := getPushshift(f, ctx, f.opt.Subreddit, f.opt.Author, before)
-		data = append(data, data2...)
-	}
-
+func (f *Fs) list(ctx context.Context, dirID string, data []api.Item) (entries fs.DirEntries, err error) {
 	var (
 		entriesMu sync.Mutex // to protect entries
+		cacheMu   sync.Mutex
 		wg        sync.WaitGroup
 		checkers  = f.ci.Checkers
 		in        = make(chan api.Item, checkers)
@@ -387,17 +309,21 @@ func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err e
 			for remote := range in {
 				u, _ := url.Parse(remote.Url)
 				file := &Object{
-					remote:    strings.Trim(u.Path, "/"),
+					remote:    path.Join(dirID, strings.Trim(u.Path, "/")),
 					fs:        f,
 					id:        remote.Id,
 					remoteUrl: remote.Url,
 				}
 				switch err := file.stat(ctx); err {
 				case nil:
+					cacheMu.Lock()
 					_, ok := f.cache[file.etag]
+					cacheMu.Unlock()
 					if !ok {
 						add(file)
-						f.cache[file.etag] = ""
+						cacheMu.Lock()
+						f.cache[file.etag] = true
+						cacheMu.Unlock()
 					}
 				default:
 					fs.Debugf(remote, "skipping because of error: %v", err)
@@ -406,29 +332,122 @@ func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err e
 		}()
 	}
 
+	var created int64
 	for _, e := range data {
-		if e.Domain == "i.redd.it" || e.Domain == "i.imgur.com" {
-			id := e.Preview.Images[0].ID
-			_, ok := f.cache[id]
+		var (
+			id string
+			ok bool
+			//d  *Dir
+		)
+		if created < e.CreatedUtc {
+			created = e.CreatedUtc
+		}
+		switch e.Domain {
+		case "i.redd.it", "i.imgur.com":
+			if len(e.Preview.Images) > 0 {
+				id = e.Preview.Images[0].ID
+				ok = f.cache[id]
+			}
 			if !ok {
-				f.cache[id] = ""
+				f.cache[id] = true
 				in <- e
 			}
-		}
-		if e.Domain == "redgifs.com" || e.Domain == "gfycat.com" {
+
+		case "redgifs.com", "gfycat.com":
 			u, _ := url.Parse(e.Url)
 			v := path.Base(u.Path)
-			_, ok := f.cache[v]
+			ok = f.cache[v]
 			if !ok {
 				e.Url = getGfyCat(f, ctx, v)
-				f.cache[v] = ""
+				f.cache[v] = true
 				in <- e
 			}
 		}
 	}
+	//f.dirCache.Put(f.opt.Subreddit, f.opt.Subreddit)
+	//d = fs.NewDir(f.opt.Subreddit, time.Unix(created, 0))
 	close(in)
 	wg.Wait()
 	return entries, nil
+}
+
+// List the objects and directories in dir into entries.  The
+// entries can be returned in any order but should be for a
+// complete directory.
+//
+// dir should be "" to list the root, and should not have
+// trailing slashes.
+//
+// This should return ErrDirNotFound if the directory isn't
+// found.
+func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err error) {
+	var (
+		data   []api.Item
+		remote string
+		u      User
+	)
+
+	/*directoryID, err := f.dirCache.FindDir(ctx, dir, false)
+	if err != nil {
+		return nil, err
+	}*/
+
+	switch dir {
+	case "":
+		for _, it := range []string{"r/", "u/"} {
+			remote = path.Join(dir, it)
+			f.dirCache.Put(remote, it)
+			d := fs.NewDir(remote, time.Now()).SetID(it)
+			entries = append(entries, d)
+		}
+		return entries, err
+	}
+
+	userDir, user := path.Split(dir)
+	if userDir == "" && user == "u" {
+		for _, user := range []string{"Alicebluwu", "yevin70335", "Touched_By_Nature"} {
+			directoryID, _ := f.dirCache.FindDir(ctx, user, false)
+
+			if directoryID == "" {
+				remote = path.Join(dir, user)
+				f.dirCache.Put(remote, user)
+				d := fs.NewDir(remote, time.Now()).SetID(user)
+				entries = append(entries, d)
+			}
+		}
+		return entries, err
+	} else if userDir == "u/" {
+		directoryID, _ := f.dirCache.FindDir(ctx, user, false)
+		u = f.users[user]
+		if directoryID == "" || len(u.entries) == 0 {
+			before := u.before
+			if before == 0 {
+				before = time.Now().Unix()
+			}
+
+			data, err = f.getPushshift(ctx, "", user, before)
+
+			if err != nil {
+				return nil, fmt.Errorf("couldn't list files: %w", err)
+			}
+
+			before = data[len(data)-1].CreatedUtc
+			u.before = before
+
+			entries, err = f.list(ctx, dir, data)
+
+			entries = append(u.entries, entries...)
+			u.entries = entries
+			f.users[user] = u
+		}
+	}
+
+	return entries, err
+
+	/*if err != nil {
+
+	}*/
+	//f.dirCache.Put(f.remote, f.opt.Subreddit)
 }
 
 // Put in to the remote path with the modTime given of the given size
@@ -443,6 +462,17 @@ func (f *Fs) Put(ctx context.Context, in io.Reader, src fs.ObjectInfo, options .
 // PutStream uploads to the remote path with the modTime given of indeterminate size
 func (f *Fs) PutStream(ctx context.Context, in io.Reader, src fs.ObjectInfo, options ...fs.OpenOption) (fs.Object, error) {
 	return nil, errorReadOnly
+}
+
+// FindLeaf finds a directory of name leaf in the folder with ID pathID
+func (f *Fs) FindLeaf(ctx context.Context, pathID, leaf string) (pathIDOut string, found bool, err error) {
+	fs.Debugf(f, "FindLeaf(%q, %q)", pathID, leaf)
+	return "", false, nil
+}
+
+// CreateDir makes a directory with pathID as parent and name leaf
+func (f *Fs) CreateDir(ctx context.Context, pathID, leaf string) (newID string, err error) {
+	return "", errorReadOnly
 }
 
 // Fs is the filesystem this remote http file object is located within
@@ -538,7 +568,7 @@ func (o *Object) stat(ctx context.Context) error {
 	o.size = parseInt64(res.Header.Get("Content-Length"), -1)
 	o.modTime = t
 	o.contentType = res.Header.Get("Content-Type")
-	o.etag = res.Header.Get("etag")
+	o.etag = strings.Trim(res.Header.Get("etag"), "\"")
 	return nil
 }
 
@@ -604,6 +634,76 @@ func (o *Object) Update(ctx context.Context, in io.Reader, src fs.ObjectInfo, op
 // MimeType of an Object if known, "" otherwise
 func (o *Object) MimeType(ctx context.Context) string {
 	return o.contentType
+}
+
+// ChangeNotify calls the passed function with a path that has had changes.
+// If the implementation uses polling, it should adhere to the given interval.
+//
+// Automatically restarts itself in case of unexpected behaviour of the remote.
+//
+// Close the returned channel to stop being notified.
+func (f *Fs) ChangeNotify(ctx context.Context, notifyFunc func(string, fs.EntryType), pollIntervalChan <-chan time.Duration) {
+	checkpoint := f.opt.Checkpoint
+
+	go func() {
+		var ticker *time.Ticker
+		var tickerC <-chan time.Time
+		for {
+			select {
+			case pollInterval, ok := <-pollIntervalChan:
+				if !ok {
+					if ticker != nil {
+						ticker.Stop()
+					}
+					return
+				}
+				if pollInterval == 0 {
+					if ticker != nil {
+						ticker.Stop()
+						ticker, tickerC = nil, nil
+					}
+				} else {
+					ticker = time.NewTicker(pollInterval)
+					tickerC = ticker.C
+				}
+			case <-tickerC:
+				checkpoint = f.changeNotifyRunner(ctx, notifyFunc, checkpoint)
+				if err := config.SetValueAndSave(f.name, "checkpoint", checkpoint); err != nil {
+					fs.Debugf(f, "Unable to save checkpoint: %v", err)
+				}
+			}
+		}
+	}()
+}
+
+func (f *Fs) changeNotifyRunner(ctx context.Context, notifyFunc func(string, fs.EntryType), checkpoint string) string {
+	var err error
+	type entryType struct {
+		path      string
+		entryType fs.EntryType
+	}
+
+	fs.Debugf(f, "Checking for changes on remote (Checkpoint %q)", checkpoint)
+	err = f.pacer.CallNoRetry(func() (bool, error) {
+		for author, u := range f.users {
+			go func(author string, u User) {
+				data, err := f.getPushshift(ctx, "", author, u.before)
+				before := data[len(data)-1].CreatedUtc
+
+				if err != nil {
+				}
+
+				entries, err := f.list(ctx, "u/"+author, data)
+				entries = append(u.entries, entries...)
+				u.entries = entries
+				u.before = before
+				f.users[author] = u
+				notifyFunc("u/"+author, fs.EntryDirectory)
+			}(author, u)
+		}
+		return false, err
+	})
+	return checkpoint
 }
 
 // Check the interfaces are satisfied
